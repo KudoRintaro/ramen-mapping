6676e6ce503eb220e79b21e71da3bb5517ac3ef4
EC2 Default User <ec2-user@ip-172-31-39-79.ap-northeast-1.compute.internal>
2023-02-11 15:12:36 +0000

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,20 @@ Rails.application.routes.draw do
    scope module: :public do
     root to: "homes#top"
     resources :shops, :except => :destroy
-    resource :users, :except => [:new, :index, :destroy] do
-      member do
-        get "/withdraw" => "users#confirm_withdraw"
-        patch "/withdraw" => "users#withdraw"
-      end 
-    end 
+    get "users/my_page" => "users#show"
+    get "users/infomation/edit" => "users#edit"
+    patch "users/infomation/edit" => "users#update"
+    get "/withdraw" => "users#confirm_withdraw"
+    patch "/withdraw" => "users#withdraw"
+    resources :genres, only: [:index, :create]
+    resources :favorite_shops, only: [:index, :create]
+    resources :comments, only: [:create, :destroy]
   end
 
   namespace :admin do
-
+    get "" => "homes#top"
   end
+
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
