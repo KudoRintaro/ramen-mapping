9d5fb226beb74642c04141b2e88292178938482d
EC2 Default User <ec2-user@ip-172-31-39-79.ap-northeast-1.compute.internal>
2023-02-11 15:00:28 +0000

    ルーティング構成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,22 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'comments/index'
-    get 'comments/show'
-    get 'comments/destroy'
-  end
-  namespace :admin do
-    get 'shops/index'
-    get 'shops/show'
-    get 'shops/edit'
-    get 'shops/update'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/destroy'
-  end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/confirm_withdraw'
-    get 'users/withdraw'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/update'
-    get 'users/confirm_withdraw'
-    get 'users/withdraw'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'favorite_shops/index'
-    get 'favorite_shops/create'
-  end
-  namespace :public do
-    get 'shops/new'
-    get 'shops/create'
-    get 'shops/index'
-    get 'shops/show'
-    get 'shops/edit'
-    get 'shops/update'
-  end
-  namespace :public do
-    get 'genres/index'
-    get 'genres/create'
-  end
-
   devise_for :admins
   devise_for :users
+
+   scope module: :public do
+    root to: "homes#top"
+    resources :shops, :except => :destroy
+    resource :users, :except => [:new, :index, :destroy] do
+      member do
+        get "/withdraw" => "users#confirm_withdraw"
+        patch "/withdraw" => "users#withdraw"
+      end 
+    end 
+  end
+
+  namespace :admin do
+
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
