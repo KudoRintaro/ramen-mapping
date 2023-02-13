26a06e701459a67525b7c683554cebf20fa6fb41
EC2 Default User <ec2-user@ip-172-31-39-79.ap-northeast-1.compute.internal>
2023-02-13 16:27:52 +0000

    会員登録機能、エラー対処中

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :image
-  
+
   validates :name, presence: true
+  validates :kana, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
