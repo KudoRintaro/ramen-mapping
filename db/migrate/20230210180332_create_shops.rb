0c0f6cc807be80daed6e6930f89274596405711b
EC2 Default User <ec2-user@ip-172-31-39-79.ap-northeast-1.compute.internal>
2023-02-20 12:18:04 +0000

    マイグレーション修正

--- db/migrate/20230210180332_create_shops.rb
+++ db/migrate/20230210180332_create_shops.rb
@@ -1,7 +1,8 @@
 class CreateShops < ActiveRecord::Migration[6.1]
   def change
     create_table :shops do |t|
-      t.references :user, type: :bigint, foreign_key: true, null: false
+      t.bigint :user, foreign_key: true, null: false
+      t.references :user
       t.references :genre, foreign_key: true, null: false
       t.string :name, null: false
       t.string :location, null: false
