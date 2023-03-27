51957e161e4b472c96d398d58a96cf42e40952b4
EC2 Default User <ec2-user@ip-172-31-39-79.ap-northeast-1.compute.internal>
2023-03-27 00:05:15 +0000

    テストコード作成

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -2,13 +2,23 @@ class Public::ReviewsController < ApplicationController
 
   def create
     shop=Shop.find(params[:shop_id])
-    Review.create(
+    review=current_user.reviews.find_by(shop_id: params[:shop_id])
+    if review.nil?
+     Review.create(
       user_id: current_user.id, shop_id: shop.id,
       atmosphere_rate: params[:review][:atmosphere_rate],
       servise_rate: params[:review][:servise_rate],
       taste_rate: params[:review][:taste_rate],
       congestion_rate: params[:review][:congestion_rate]
-    )
+     )
+    else
+     review.update(
+      atmosphere_rate: params[:review][:atmosphere_rate],
+      servise_rate: params[:review][:servise_rate],
+      taste_rate: params[:review][:taste_rate],
+      congestion_rate: params[:review][:congestion_rate]
+     )
+    end
     redirect_to request.referer
   end
 
---
+++ spec/factories/shop.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :shop do
+    name {Faker::Lorem.characters(number:10)}
+    location {Faker::Lorem.characters(number:30) }
+    business_start_hours {Faker::Time.at(rand*Time.now.to_i)}
+    business_finish_hours {Faker::Time.at(rand*Time.now.to_i)
+    genre {association :genre}
+    user {associacion :user}
+    holiday {Faker::Base.sample(["月曜日","火曜日","水曜日","木曜日","金曜日","土曜日","日曜日"])
+  end
+end
